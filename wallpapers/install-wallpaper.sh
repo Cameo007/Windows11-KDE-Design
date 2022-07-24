4333cad6eb12df28b21bc2c07294c039bebb4338
Pascal Dietrich <80521473+Cameo007@users.noreply.github.com>
2022-07-24 14:54:27 +0200

    Update install-wallpaper.sh

--- wallpapers/install-wallpaper.sh
+++ wallpapers/install-wallpaper.sh
@@ -2,7 +2,6 @@
 
 SRC_DIR=$(cd $(dirname $0) && pwd)
 
-mode="";
 color="";
 
 if [ "$UID" = "0" ]
@@ -15,21 +14,15 @@ fi
 while getopts m:c: flag
 do
     case "${flag}" in
-        m) mode="$OPTARG";;
         c) color="$OPTARG";;
     esac
 done
 
-if [ "$mode" != "light" ] && [ "$mode" != "dark" ]
-then
-    echo "Please specify the mode (light/dark)"
-    exit 1
-fi
-
-if ! [ -f "$SCRIPT_DIR/$mode/$color.jpg" ]
+if ! [ -f "$SRC_DIR/light/$color.jpg" ]
 then
     echo "Wallpaper \"$color\" not found"
     exit 1
 fi
 
-cp -r $SRC_DIR/$mode/$color.jpg $WALLPAPER_DIR/Win11OS-$mode/contents/images/3840x2400.jpg
+cp -r $SRC_DIR/light/$color.jpg $WALLPAPER_DIR/Win11OS-light/contents/images/3840x2400.jpg
+cp -r $SRC_DIR/dark/$color.jpg $WALLPAPER_DIR/Win11OS-dark/contents/images/3840x2400.jpg
