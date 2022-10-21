d2843247e9011953ca418a041011d7d6207302da
Pascal Dietrich <80521473+Cameo007@users.noreply.github.com>
2022-10-21 16:35:25 +0200

    Update change-wallpaper.sh

--- wallpaper/change-wallpaper.sh
+++ wallpaper/change-wallpaper.sh
@@ -18,20 +18,20 @@ do
     esac
 done
 
-if ! [ -f "./Variants/light/$color.jpg" ]
+if ! [ -f "./wallpaper/Variants/light/$color.jpg" ]
 then
     echo "Wallpaper \"$color\" not found"
     exit 1
 fi
 
 #Copy desktop wallpaper
-cp -r ./Variants/light/$color.jpg $WALLPAPER_DIR/Win11OS-light/contents/images/3840x2400.jpg
-cp -r ./Variants/dark/$color.jpg $WALLPAPER_DIR/Win11OS-dark/contents/images/3840x2400.jpg
+cp -r ./wallpaper/Variants/light/$color.jpg $WALLPAPER_DIR/Win11OS-light/contents/images/3840x2400.jpg
+cp -r ./wallpaper/Variants/dark/$color.jpg $WALLPAPER_DIR/Win11OS-dark/contents/images/3840x2400.jpg
 
 #Copy splash screen wallpaper
-cp -r ./Variants/light/$color.jpg $LOOKFEEL_DIR/com.github.Cameo007.Windows11-KDE-Design-Light/contents/splash/images/background.jpg
-cp -r ./Variants/dark/$color.jpg $LOOKFEEL_DIR/com.github.Cameo007.Windows11-KDE-Design-Dark/contents/splash/images/background.jpg
+cp -r ./wallpaper/Variants/light/$color.jpg $LOOKFEEL_DIR/com.github.Cameo007.Windows11-KDE-Design-Light/contents/splash/images/background.jpg
+cp -r ./wallpaper/Variants/dark/$color.jpg $LOOKFEEL_DIR/com.github.Cameo007.Windows11-KDE-Design-Dark/contents/splash/images/background.jpg
 
 echo "Please enter your password for changing the SDDM wallpaper if you aren't authenticated"
 #Run script for copying SDDM wallpaper
-exec sudo "./change-sddm-wallpaper.sh" "$@"
+exec sudo "./wallpaper/change-sddm-wallpaper.sh" "$@"
